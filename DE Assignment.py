e30d2e7659c007184711aef0e5e8cac17247d541
Nikola Novarlic <35609096+greendate@users.noreply.github.com>
2018-11-04 17:59:54 +0300

    Add files via upload

--- DE Assignment.py
+++ DE Assignment.py
@@ -8,18 +8,18 @@ x0 = 0
 y0 = 1
 b = 9
 n = 48
-l = 0  # left border
-r = 48  # right border
+n1 = 10  # start of the interval
+n2 = 48  # end of the interval
 
 
-def cnt():
-    global x0, y0, b, n, l, r
+def cnt():  # function that takes values which user entered after pressing 'continue'
+    global x0, y0, b, n, n1, n2
     x0 = int(var_x0.get())
     y0 = int(var_y0.get())
     b = int(var_b.get())
     n = int(var_n.get())
-    l = int(var_l.get())
-    r = int(var_r.get())
+    n1 = int(var_l.get())
+    n2 = int(var_r.get())
     root.destroy()
     return
 
@@ -82,14 +82,15 @@ def runge_kutta_method(x0, y0, b, n):
     return y
 
 
-def compute_error(method1, method2):
-    answer = (r - l) * [0]
-    for i in range(l, r):
-        answer[i - l] = method1[i] - method2[i]
+def compute_error(method1, method2):  # compute difference between numerical method and exact solution at each step
+    m = len(method1)
+    answer = m * [0]
+    for i in range(m):
+        answer[i] = method1[i] - method2[i]
     return answer
 
 
-def investigate_convergence(error):
+def investigate_convergence(error):  # check if any error doesn't tend to 0, in that case method is not convergent
     for element in error:
         if round(element) != 0:
             print(" is not convergent.")
@@ -114,6 +115,30 @@ def plotErrors(error1, error2, error3):
     plt2.legend([err_line1, err_line2, err_line3],
                 ['Euler method error', 'Improved Euler method error', 'Runge-Kutta method error'])
     plt2.show()
+
+
+def plot_total_errors():
+    total_errors_e = []  # Euler
+    total_errors_ie = []  # Improved Euler
+    total_errors_rk = []  # Runge-Kutta
+    for i in range(n1, n2):
+        # values of exact solution and numerical methods when we use i computational steps
+        exact_i = exact_solution(x0, y0, b, i)
+        euler_i = euler_method(x0, y0, b, i)
+        improved_i = improved_euler_method(x0, y0, b, i)
+        runge_kutta_i = runge_kutta_method(x0, y0, b, i)
+
+        # compute their errors
+        error1_i = compute_error(exact_i, euler_i)
+        error2_i = compute_error(exact_i, improved_i)
+        error3_i = compute_error(exact_i, runge_kutta_i)
+
+        # append average values of errors
+        total_errors_e.append(sum(error1_i) / i)
+        total_errors_ie.append(sum(error2_i) / i)
+        total_errors_rk.append(sum(error3_i) / i)
+
+    plotErrors(total_errors_e, total_errors_ie, total_errors_rk)
 
 
 #------------------- graphic user interface -----------------#
@@ -141,18 +166,18 @@ l4.grid(row=3, column=0)
 var_n = Entry(root)
 var_n.grid(row=3, column=1)
 
-l5 = Label(root, text="Point where you want to start range: ")
+l5 = Label(root, text="N1: ")
 l5.grid(row=4, column=0)
 var_l = Entry(root)
 var_l.grid(row=4, column=1)
 
-l5 = Label(root, text="Point where you want to finish range: ")
+l5 = Label(root, text="N2: ")
 l5.grid(row=5, column=0)
 var_r = Entry(root)
 var_r.grid(row=5, column=1)
 
 button = Button(root, text="Continue", command=cnt)
-button.grid(row=6, column=0)
+button.grid(row=6, column=1)
 root.mainloop()
 #-------------------------------------------------------------#
 
@@ -169,6 +194,9 @@ error3 = compute_error(exact, runge_kutta)
 
 plotErrors(error1, error2, error3)
 
+plot_total_errors()
+
+# output convergence of methods
 print("Euler method", end="")
 investigate_convergence(error1)
 print("Improved Euler method", end="")
